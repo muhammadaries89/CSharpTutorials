02c0eead69ec9289a9cc0842597001d2eb17e5a8
muhammadaries89 <64781468+muhammadaries89@users.noreply.github.com>
2020-05-04 22:59:29 +0500

    Tutorial 2 Data Types

---
+++ Tutorial 2 [Data Types]/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Integer Data Type
+            Int32 intVal = 25 + 5;
+            Console.WriteLine(intVal);
+
+            //double
+            double doubleValue = 12.5;
+            Console.WriteLine(doubleValue);
+
+            //boolean
+            Boolean boolVal = true;
+            Console.WriteLine(boolVal);
+
+            //string
+            String stringVal = "Test String";
+            Console.WriteLine(stringVal);
+
+            Console.Read();
+        }
+    }
+}
